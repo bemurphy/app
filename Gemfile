7c1e3dd27547e1fc7cd9e34c8587a85e75935cb4
Brendon Murphy <xternal1+github@gmail.com>
2014-06-01 10:05:33 -0700

    Pull in app structure generated by gn
    I generated an app from gn, and then just moved what was generated
    back to this repo.  This is because I'm not really using the template
    features gn provides anymore, so it's easier to just use the files
    I want.  It makes editing easier.

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source "https://rubygems.org"
+
+gem "cuba"
+gem "cuba-contrib"
+gem "mote"
+gem "ohm"
+gem "ohm-contrib"
+gem "rack-protection"
+gem "scrivener"
+gem "scrivener_errors"
+gem "shield"
+
+group :development do
+  gem "shotgun"
+end
---
+++ app.rb
@@ -0,0 +1,40 @@
+require "cuba"
+require "cuba/contrib"
+require "mote"
+require "ohm"
+require "ohm/contrib"
+require "rack/protection"
+require "scrivener"
+require "scrivener_errors"
+require "shield"
+
+Cuba.plugin Cuba::Mote
+Cuba.plugin Shield::Helpers
+
+# Require all application files.
+Dir["./models/**/*.rb"].each  { |rb| require rb }
+Dir["./routes/**/*.rb"].each  { |rb| require rb }
+
+# Require all helper files.
+Dir["./helpers/**/*.rb"].each { |rb| require rb }
+Dir["./filters/**/*.rb"].each { |rb| require rb }
+
+Cuba.use Rack::MethodOverride
+Cuba.use Rack::Session::Cookie,
+  key: "my_new_app",
+  secret: ENV.fetch("SESSION_SECRET")
+
+Cuba.use Rack::Protection
+Cuba.use Rack::Protection::RemoteReferrer
+
+Cuba.use Rack::Static,
+  root: "./public",
+  urls: %w[/js /css /img]
+
+Cuba.define do
+  persist_session!
+
+  on root do
+    render('index')
+  end
+end
